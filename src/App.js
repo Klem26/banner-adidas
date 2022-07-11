606f64fcf9325bb5169ae86693960a516589abee
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2022-07-11 16:20:01 +0300

    added banner

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="background">
+          <div className="galalxy_bg">
+            <div className="discovery_wing">
+              <div className="cta-medium"></div>
+            </div>
+            <div className="tfp"></div>
+            <div className="shoe_position"></div>
+            <div className="ultraboost"></div>
+            <div className="logo_black"></div>
+            <div className="goodbye"></div>
+            <div className="gravity"></div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
